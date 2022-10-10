6bf5046f8fed758e1b8fba9075d6eba560bd36b3
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2022-10-10 21:41:32 +0200

    implement library constant

---
+++ packages/core/src/library/constant/alertText/listQuotes/index.js
@@ -0,0 +1,15 @@
+export const alertTextListQuotes = Object.freeze({
+  pagination: { browsing: 'Browsing through list quotes...' },
+  display: {
+    noQuotes: 'No list quotes!',
+  },
+  action: {
+    search: {
+      progress: 'Searching quotes...',
+    },
+    set: {
+      progress: 'Setting list quotes...',
+      success: 'Successfully set list quotes!',
+    },
+  },
+})
---
+++ packages/core/src/library/constant/index.js
@@ -0,0 +1,32 @@
+export const pageSize = Object.freeze({
+  quotes: 7,
+})
+
+export const pageNumber = 1
+export const maxRating = 100
+
+export const eventKey = Object.freeze({
+  enter: 'Enter',
+  escape: 'Escape',
+})
+
+export const timeout = Object.freeze({
+  alert: 1000,
+  fetch: 500,
+})
+
+export const messageType = Object.freeze({
+  info: 'info',
+  warning: 'warning',
+  error: 'error',
+  success: 'success',
+  consent: 'consent',
+})
+
+export const pageContent = Object.freeze({
+  listQuote: {
+    subheadHeadingLeft: 'View Single Random Quote',
+    subheadHeadingRight: 'Search Random Quotes',
+    contextNavigationItemText: 'List Quotes',
+  },
+})
