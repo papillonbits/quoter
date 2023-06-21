4a7b9b06befac2c060665d16e25088301485ff55
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2023-06-21 18:50:54 +0200

    bumpstack quoter comply with node v20.3.1 npm v9.6.7

--- packages/core/bumpstack.sh
+++ packages/core/bumpstack.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-npm un -D nodemon webpack webpack-dev-server && npm i -D nodemon webpack@^4.46.0 webpack-dev-server@^3.11.2
-
-npm run clean-up && npm run install-packages && npm audit fix
+npm run clean-up && npm run install-packages
